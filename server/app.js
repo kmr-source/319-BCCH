0c14c3067b2b29e19d5e0c5b397254cbc3318f9c
jiaweitan <gamilsphat@gmail.com>
2020-01-16 16:31:21 -0800

    gitignore and server path

--- server/app.js
+++ server/app.js
@@ -4,6 +4,6 @@ const path = require('path');
 
 const port = 3000;
 server.use('/assets', express.static(path.resolve(__dirname, "../web/dist")));
-server.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "../web/dist/index.html")));
+server.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "../web/src/index.html")));
 
 server.listen(port, () => console.log(`listening port ${port}`));
