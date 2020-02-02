ddd52cc5c8734c17f8b8ce9de29e19eb1dfd34f2
LangLangSAMA <lang.chlorine@gmail.com>
2020-02-02 13:00:35 -0800

    add css loader, setup react app

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ let server = express();
 const path = require('path');
 
 const port = 3000;
-server.use('/assets', express.static(path.resolve(__dirname, "../web/dist")));
-server.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "../web/src/index.html")));
+server.use('/assets', express.static(path.resolve(__dirname, "../web/public")));
+server.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "../web/public/index.html")));
 
 server.listen(port, () => console.log(`listening port ${port}`));
---
+++ web/src/component/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+// import components
+
+// import css
+import "./App.scss";
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    render() {
+        return (
+            <div className="component-app">
+                Hello
+            </div>
+        );
+    }
+}
+export default App;
