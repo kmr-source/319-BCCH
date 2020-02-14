46fe763d735b22f676786bb21207ada223d6a631
raymond.chen <raymondyao1997@gmail.com>
2020-02-14 15:24:57 -0800

    changed the compile path to target, reformatted the backend structure

--- server/src/App.ts
+++ server/src/App.ts
@@ -4,9 +4,8 @@ import * as path from 'path';
 let server = express();
 
 const port: number = 3000;
-server.use('/assets', express.static(path.resolve(__dirname, "../web/public")));
+server.use('/assets', express.static(path.resolve(__dirname, "../../web/public")));
 // handle every other route with index.html, which will contain
 // a script tag to your application's JavaScript file(s).
-server.get('*', (req: any, res: any) => res.sendFile(path.resolve(__dirname, "../web/public/index.html")));
-
+server.get('*', (req: any, res: any) => res.sendFile(path.resolve(__dirname, "../../web/public/index.html")));
 server.listen(port, () => console.log(`listening port ${port}`));
