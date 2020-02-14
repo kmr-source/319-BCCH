de3211c88151e59aa9fb9116a3ec9352d800ba1e
Lang Cheng <lang.chlorine@gmail.com>
2020-02-14 05:03:23 +0000

    Login page

--- server/app.ts
+++ server/app.ts
@@ -5,6 +5,8 @@ let server = express();
 
 const port: number = 3000;
 server.use('/assets', express.static(path.resolve(__dirname, "../web/public")));
-server.get('/', (req: any, res: any) => res.sendFile(path.resolve(__dirname, "../web/public/index.html")));
+// handle every other route with index.html, which will contain
+// a script tag to your application's JavaScript file(s).
+server.get('*', (req: any, res: any) => res.sendFile(path.resolve(__dirname, "../web/public/index.html")));
 
 server.listen(port, () => console.log(`listening port ${port}`));
---
+++ web/src/component/Dashboard.jsx
@@ -0,0 +1,5 @@
+import React, { useState } from "react";
+
+export function Dashboard() {
+  return <h2>Dashboard</h2>;
+}
