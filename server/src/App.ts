fad9864190e52eee9314636dab6860f7b4651504
Alexander S <asvatukhin@gmail.com>
2020-02-19 04:18:51 +0000

    Server

--- server/src/App.ts
+++ server/src/App.ts
@@ -1,11 +1,64 @@
 import * as express from "express";
-import * as path from 'path';
+import * as path from "path";
+
+const port: number = 3000;
 
 let server = express();
 
-const port: number = 3000;
+// parse application/json
+server.use(require("body-parser").json());
 server.use('/assets', express.static(path.resolve(__dirname, "../../web/public")));
+
+server.get('/', (req, res) => {
+    res.send('Welcome!');
+});
+
+interface User {
+    username: string;
+    password: string;
+    type?: string;
+}
+
+const users: User[] = [
+    {
+        username: "admin",
+        password: "admin",
+        type: "admin"
+    }, {
+        username: "lang",
+        password: "123",
+        type: "user"
+    }, {
+        username: "raymond",
+        password: "123",
+        type: "user"
+    },
+]
+
+server.get('/login', (req, res) => {
+    console.log(req.body);
+    let body: User = req.body;
+    if (body.username == null || body.password == null) {
+        res.status(400).send({ error: "Missing required parameters" });
+        return;
+    }
+
+    for (const u of users) {
+        if (u.username === body.username && u.password === body.password) {
+            res.send({ username: u.username, type: u.type });
+            return;
+        }
+    }
+
+    res.status(404).send({ error: "Invalid credentials" });
+});
+
+server.get('/upload', (req, res) => {
+    res.send();
+});
+
 // handle every other route with index.html, which will contain
 // a script tag to your application's JavaScript file(s).
 server.get('*', (req: any, res: any) => res.sendFile(path.resolve(__dirname, "../../web/public/index.html")));
+
 server.listen(port, () => console.log(`listening port ${port}`));
