f5807190485f75633827ab218b3fd2452573d483
kthnd <asvatukhin@gmail.com>
2020-03-05 15:18:02 -0800

    Update port number

--- server/src/App.ts
+++ server/src/App.ts
@@ -9,7 +9,7 @@ import { LoginController } from "./controllers/LoginController";
 import { AssessmentController } from "./controllers/AssessmentController";
 import { Controller } from "./controllers/Controller";
 
-const port: number = 3000;
+const port = process.env.PORT || 3000;
 
 let server = express();
 
