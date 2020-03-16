65555639c05a9450a814b544d0698def85fdc29a
kmr-source <kaitimokrong@hotmail.com>
2020-03-16 21:49:54 +0000

    Update AssessmentController.ts - Done getAssessment()

--- server/src/controllers/AssessmentController.ts
+++ server/src/controllers/AssessmentController.ts
@@ -9,26 +9,56 @@ export class AssessmentController extends AuthController {
     
         let db = DBConnection.getInstance(); 
         let res = null;
-        if (t === "string") {
+        if (t !== "string") {
             res = await db.send("Select From AssessmentTemplace WHERE id=? AND is_archived=1",[t]);
         } else {
             res = await db.send("Select From AssesmentTemplate WHERE title=? ANd is_isarchived=1",[t]);
         }
-        let info = res.body;
-        // set up wanted for assesment.
-        let assessmentForm = {
-            title: info.name,
-            id: info.id,
-            desc: info.desc,
-            vidoes:info.vidoes,
-            pictures: info.pictures,
-            surveys: info.surveys,
-        
-        };
-        this.response.status(200).send(assessmentForm);
-        return; 
-    }
+        if (res.length !== 0) {
+            let info = res[0];
 
+            let atitle = info.name;
+            let idName = info.id;
+            let tempAssessWithOnlySurveyIDs = await AssessmentTemplateImpl.getById(idName);
+            
+            let descp = tempAssessWithOnlySurveyIDs.description;
+            let videoDesc = tempAssessWithOnlySurveyIDs.videos;
+            let picturesDesc = tempAssessWithOnlySurveyIDs.pictures;
+           
+            let surveyIDs = tempAssessWithOnlySurveyIDs.surveyIDs;
+            let surveyInfos = await SurveyTemplateImpl.getByIds(surveyIDs);
+            let surveyResults: any[] = [];
+            for (let i of surveyInfos) {
+                let template: any = {};
+                template.Title = i.name;
+                template.sId = i.id;
+                template.sInst = i.inst;
+                let allQuestion : any[] = [];
+                for (let q of i.questions) {
+                    let surveyQues = {
+                    qOrder : q.number,
+                    qDesc : q.statement,
+                    qType : q.type,
+                    qOpts : q.meta,
+                    };
+                    allQuestion.push(surveyQues);
+                }
+                template.sContent = allQuestion;
+                surveyResults.push(template);
+            }
+            let assessmentForm = {
+                title: atitle,
+                id: idName,
+                desc: descp,
+                vidoes: videoDesc,
+                pictures: picturesDesc,
+                surveys: surveyResults,
+            
+            };
+            return this.response.status(200).send(assessmentForm);
+        }
+        return this.response.status(400).send( {error: "Invalid input"});
+    }
     // just send all AssesmentTemplates name and ID
     async getAllAssessments() { 
         let db = DBConnection.getInstance();
